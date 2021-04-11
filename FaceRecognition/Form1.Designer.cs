fece9192e840701c9db3913acdc60ff306d428ea
arti021998 <arti021998@gmail.com>
2021-04-11 18:12:38 +0200

    Добавьте файлы проекта.

---
+++ FaceRecognition/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿
+namespace FaceRecognition
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		/// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Код, автоматически созданный конструктором форм Windows
+
+		/// <summary>
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+			this.pictureBox1.Location = new System.Drawing.Point(0, 2);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(630, 320);
+			this.pictureBox1.TabIndex = 0;
+			this.pictureBox1.TabStop = false;
+			this.pictureBox1.UseWaitCursor = true;
+			this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+			// 
+			// button1
+			// 
+			this.button1.FlatStyle = System.Windows.Forms.FlatStyle.System;
+			this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.button1.Location = new System.Drawing.Point(661, 371);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(115, 49);
+			this.button1.TabIndex = 1;
+			this.button1.Text = "Detect";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.UseWaitCursor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// openFileDialog1
+			// 
+			this.openFileDialog1.FileName = "openFileDialog1";
+			this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(813, 436);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.pictureBox1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.Name = "Form1";
+			this.Text = "FaceRecogn";
+			this.UseWaitCursor = true;
+			this.Load += new System.EventHandler(this.Form1_Load);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.OpenFileDialog openFileDialog1;
+	}
+}
+
---
+++ FaceRecognition/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.Structure;
+
+namespace FaceRecognition
+{
+	public partial class Form1 : Form
+	{
+		private static CascadeClassifier classifier = new CascadeClassifier("haarcascade_frontalface_alt_tree.xml");
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void pictureBox1_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			try
+			{
+				DialogResult res = openFileDialog1.ShowDialog();
+				if (res.Equals(DialogResult.OK))
+				{
+					string path = openFileDialog1.FileName;
+					pictureBox1.Image = Image.FromFile(path);
+
+					Bitmap bitmap = new Bitmap(pictureBox1.Image);
+					Image<Bgr,byte> grayImage = new Image<Bgr, byte>(bitmap);
+
+					Rectangle[] faces = classifier.DetectMultiScale(grayImage,1.01,2);
+					Console.WriteLine(faces.Length);
+					foreach(Rectangle face in faces)
+					{
+						Console.WriteLine(face.Width);
+						Console.WriteLine(face.Height);
+						using (Graphics graphics = Graphics.FromImage(bitmap))
+						{
+							using(Pen pen=new Pen(Color.Aquamarine, 2))
+							{
+								graphics.DrawRectangle(pen, face);
+							}
+						}
+					}
+					pictureBox1.Image = bitmap;
+				}
+				else
+				{
+					MessageBox.Show("No image selected", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+				}
+			}
+			catch(Exception ex)
+			{
+				MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+			}
+
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+
+		}
+
+		private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+		{
+
+		}
+	}
+}
